1a5cba3aee0dbdba7adeb250b37966419d9f0bf0
shubham pathak <95shubham1@gmail.com>
2021-01-07 12:39:39 +0530

    some changes failed for 0 test case neeed to review

--- first_negtive_in_window_size_fix.cpp
+++ first_negtive_in_window_size_fix.cpp
@@ -21,28 +21,29 @@ void slider(int a[],int n,int k)
 		}
 		else if(j-i+1==k)
 		{
-			if(q.size()==0)
+			if(q.empty())
 			result.push_back(0);//	cout<<q.size();
-				else
+			else
 			{
-				cout<<q.front();
+			//	cout<<q.front();
 				result.push_back(q.front());
 				
 				if(a[i]==q.front())
 				{
 			//	cout<<"dc";	
 				q.pop();
+			//	cout<<q.size();
 		//		cout<<q.front();	
 				}
 				i++;
-				j--;
+				j++;
 			}
 		}
 	}
 	
 	for(auto x:result)
 	{
-	//	cout<<x<<" ";
+		cout<<x<<" ";
 	}
 }
 
@@ -50,11 +51,11 @@ int main()
 {
  //cout<<"Hello";
  
- int a[]={-4,3,-3,1};
+ int a[]={-4,3,-3,1,2,-6,2,2,1};
  
  int n=sizeof(a)/sizeof(a[0]);
  
- int k=2;
+ int k=3;
  
  slider(a,n,k);
 }
