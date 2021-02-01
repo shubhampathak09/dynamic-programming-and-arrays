fa0aadf7e1f26eb33f1e0e9d12fcd84b578bce4a
shubham pathak <95shubham1@gmail.com>
2021-02-01 10:33:16 +0530

    get all balanced paranhesis

---
+++ CP guide/generate_all_balanced_paranthesis.cpp
@@ -0,0 +1,45 @@
+// generate all balanced paranthesis
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve(string out,int op,int cl,vector<string>&res)
+{
+	if(op==0&&cl==0)
+	{
+		res.push_back(out);
+		return;
+	}
+	
+	if(op!=0)
+	{
+		string out1=out;
+		out1.push_back('(');
+		solve(out1,op-1,cl,res);
+	}
+	if(cl>op)
+	{
+		string out2=out;
+		out2.push_back(')');
+		solve(out2,op,cl-1,res);
+	}
+}
+
+int main()
+{
+	
+	int n=3;
+	int op=n;
+	int cl=n;
+	
+	vector<string>result;
+	string out="";
+	
+	solve(out,op,cl,result);
+	
+	for(auto x:result)
+	{
+		cout<<x<<endl;
+	}
+}
