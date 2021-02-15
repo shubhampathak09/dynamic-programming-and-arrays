655e97eae833543f679c6988b18d776e2c4c6943
shubham pathak <95shubham1@gmail.com>
2021-02-15 19:44:36 +0530

    some

---
+++ CP guide/stack helper for histogra,.cpp
@@ -0,0 +1,60 @@
+// max area histogram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve(int a[],int n)
+{
+	
+	stack<int>s;
+	int left[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		
+		if(s.empty())
+		{
+			s.push(i);
+			left[i]=i;
+		}
+		else
+		{
+		
+		while(a[i]<=a[s.top()]&&s.empty()==false)
+		{
+			s.pop();
+			 
+			
+		}
+			if(s.empty())
+		{
+			left[i]=0;
+			s.push(i);
+		}
+		else
+		{
+			left[i]=a[s.top()+1];
+			s.push(i);
+		}
+	}
+		
+	
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		cout<<left[i]<<" ";
+	}
+}
+
+int main()
+{
+	
+	int a[]={2,1,5,6,2,3};
+	
+	int n=sizeof(a)/sizeof(a[0]);
+	
+	solve(a,n);
+	
+}
