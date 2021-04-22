59c218e452d357dbfdfead1b5eef2971f748113b
shubham pathak <95shubham1@gmail.com>
2021-04-22 20:10:33 +0530

    minimum spanning tree

---
+++ CP guide/prim mst.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define V 5
+
+
+
+int minKey(int key[],bool mstset[])
+{
+	
+	int min=INT_MAX;
+	int min_index;
+	
+	for(int v=0;v<V;v++)
+	{
+		if(mstset[v]==false&&key[v]<min)
+		{
+			min=key[v];
+			min_index=v;
+		}
+	}
+	
+	return min_index;
+}
+
+void printmst(int parent[],int graph[V][V])
+{
+	
+	for(int i=1;i<V;i++)
+	{
+		cout<<parent[i]<<" "<<i<<" "<<graph[i][parent[i]];
+		cout<<endl;
+	}
+	
+}
+
+void primmst(int graph[V][V])
+{
+	
+	int parent[V];
+	
+	bool mstset[V];
+	
+	int key[V];
+	
+	for(int i=0;i<V;i++)
+	{
+		
+		key[i]=INT_MAX;
+		mstset[i]=false;
+	}
+	
+	
+	key[0]=0;
+	parent[0]=-1;
+	
+	for(int i=1;i<=V;i++)
+	{
+		
+		int u=minKey(key,mstset);
+		mstset[u]=true;
+		
+		for(int i=0;i<V;i++)
+		{
+			if(graph[u][i]&&mstset[i]==false&&graph[u][i]<key[i])
+			{
+				parent[i]=u;
+				key[i]=graph[u][i];
+			}
+		}
+		
+	}
+	
+	
+	printmst(parent,graph);
+}
+
+
+int main()
+{
+	
+	  int graph[V][V] = { { 0, 2, 0, 6, 0 }, 
+                        { 2, 0, 3, 8, 5 }, 
+                        { 0, 3, 0, 0, 7 }, 
+                        { 6, 8, 0, 0, 9 }, 
+                        { 0, 5, 7, 9, 0 } }; 
+                        
+                        
+
+primmst(graph);
+
+
+	
+}
