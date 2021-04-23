de5c51319f131b61ce56a20f618ce98d896088bb
shubham pathak <95shubham1@gmail.com>
2021-04-23 13:00:21 +0530

    range xor segment tree

---
+++ CP guide/range xor.cpp
@@ -0,0 +1,49 @@
+// range xor 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int>a(10001);
+vector<int>seg(4*10001);
+
+void build(int si,int left,int right)
+{
+
+if(left==right)
+{
+	seg[si]=a[left];
+	return;
+	}	
+int mid=(left+right)/2;
+	
+build(2*si+1,left,mid);
+build(2*si+2,mid+1,right);
+seg[si]=seg[2*si+1]^seg[2*si+2];
+	
+}
+
+int query(int si,int rl,int rr,int left,int right)
+{
+	
+	if(left>=rl&&right<=rr)
+	return seg[si];
+	
+	if(right<rl||left>rr)
+	return 0;
+	
+	int mid=(left+right)/2;
+	return query(2*si+1,rl,rr,left,mid)^query(2*si+2,rl,rr,mid+1,right);
+	
+}
+
+int main()
+{
+	
+	a={2,3,4,5,6,1};
+	
+	int n=6;
+	
+	build(0,0,n-1);
+	
+	cout<<query(0,2,3,0,n-1);
+}
