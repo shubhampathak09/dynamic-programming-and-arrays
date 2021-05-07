3d8c729601fe2abad94fc79e9e9ceac46eaa9ca0
shubham pathak <95shubham1@gmail.com>
2021-05-07 13:40:54 +0530

    codes

---
+++ dynamic programing imp questions/cyclic shift array elements by 1.cpp
@@ -0,0 +1,37 @@
+// cyclic shift the array elements by 1
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	
+	
+	int a[]={1,2,5,1,0,9};
+	
+	cout<<endl;
+	
+	int n=sizeof(a)/sizeof(a[0]);
+	
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	
+	cout<<endl;
+	int temp=a[n-1];
+	
+	for(int i=n-1;i>=1;i--)
+	{
+		a[i]=a[i-1];
+	}
+	a[0]=temp;
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
