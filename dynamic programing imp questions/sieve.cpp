76df08e72f60c65e70aa27dc7ce4f258625a8097
shubham pathak <95shubham1@gmail.com>
2021-05-12 18:12:18 +0530

    codes

---
+++ dynamic programing imp questions/sieve.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void sieve()
+{
+	
+	int n=101;
+	int isprime[n];
+	
+	for(int i=0;i<101;i++)
+	isprime[i]=1;
+	
+	isprime[0]=isprime[1]=0;
+	
+	for(int i=2;i<=sqrt(100);i++)
+	{
+		
+		for(int j=i*i;j<=100;j=j+i)
+		isprime[j]=0;
+	}
+	int cnt=0;
+	for(int i=2;i<=100;i++)
+	{
+		if(isprime[i])
+		{
+		cnt++;
+		cout<<i<<endl;
+	}
+	}
+	cout<<cnt;
+}
+
+
+int main()
+{
+	
+	sieve();
+	
+}
