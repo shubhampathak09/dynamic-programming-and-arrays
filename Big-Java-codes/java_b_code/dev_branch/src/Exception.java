9afaa527edca9e89a1c5ae5b5131ed3b333dd965
shubham pathak <95shubham1@gmail.com>
2021-05-21 18:31:13 +0530

    Java exception

---
+++ Big-Java-codes/java_b_code/dev_branch/src/Exception.java
@@ -0,0 +1,45 @@
+import com.sun.org.apache.xalan.internal.xsltc.runtime.ErrorMessages_sk;
+import sun.plugin2.liveconnect.JSExceptions;
+
+import java.io.EOFException;
+import java.io.InterruptedIOException;
+import java.rmi.RemoteException;
+import java.sql.Types;
+
+public class Exception {
+
+    Types:-
+    Compile time exception and Run time exception.
+
+    Throwable ->Exception.class
+    Throwbale->Errors
+
+    Compiel Time exception
+    ClassNotFoundException
+    NoSuchMethodException
+    SQl Exception
+    RemoteException
+    InterruptException
+
+    IO Exception
+       -EOFException
+       FileNotFoundException
+       InterruptedIOException
+       Compile time JSExceptions
+
+  Runtime Exception
+       Arithmetic Exception
+       NullPointer Execption
+       IndexOutOfBoundsException
+       IlegalArgument
+       ClassCastException
+
+    and erros
+     VirtualMachineError
+     AssertionError
+     StackOverflowError
+     OutOfMemoryError
+     LinkageError
+
+
+}
